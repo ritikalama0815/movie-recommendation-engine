518149289f34426f2ccb1907d5f17e05055035b7
Ritika Lama <ritikatheeng2002@gmail.com>
2026-05-27 21:51:20 -0500

    count vectorizer and similarity

--- movieRecommendation.py
+++ movieRecommendation.py
@@ -17,5 +17,27 @@ print(df.head())
 print(df.columns)
 
 
-##Step 2: Select Features
+##Step 2: Select "important" columns
 
+important_features = ["keywords", "cast", "genres", "director"]
+
+for feature in important_features:
+	df[feature] = df[feature].fillna("") # fill NaN values with empty string cuz we got error below before
+# Combines params into one string
+def combine_features(row):
+	try:
+		return row["keywords"] + " " + row["cast"] + " " + row["genres"] + " " + row["director"]
+	except:
+		print("Error:", row)
+
+df["combined_features"] = df.apply(combine_features, axis=1) # axis=1 means apply function to each row
+
+print(df["combined_features"].head())
+
+##Step 3: Create count matrix from this new combined column
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(df["combined_features"])
+
+similarity_scores = cosine_similarity(count_matrix)
+
+movie_user_likes = input("Enter your favourite movie: ")
